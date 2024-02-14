a0abb8708dc9c46fdc2bf1354244b27070b82d6c
Merey <kemelm2004@gmail.com>
2024-02-14 21:31:50 +0600

    operators/the postfix and prefix forms

---
+++ Lab3/tasks1/5postandprefixfrom.js
@@ -0,0 +1,8 @@
+//What are the final values of all variables a, b, c and d after the code below?
+let a = 1, b = 1;
+
+alert( ++a ); // 2, prefix form returns the new value
+alert( b++ ); // 1, postfix form returns the old value
+
+alert( a ); // 2, incremented once
+alert( b ); // 2, incremented once
