1daaa26e914d34524a1d5acb242482487bbea6e3
Merey <kemelm2004@gmail.com>
2024-02-14 23:32:39 +0600

    comparisons/*2

---
+++ Lab3/tasks1/9comparisons.js
@@ -0,0 +1,8 @@
+//What will be the result for these expressions?
+5 > 4 //True
+"apple" > "pineapple" //false Dictionary comparison
+"2" > "12" //true Dictionary comparison
+undefined == null //true
+undefined === null //false different types
+null == "\n0\n" //false
+null === +"\n0\n" //false
