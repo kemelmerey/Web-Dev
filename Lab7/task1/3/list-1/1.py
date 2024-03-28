c055ef9226daea4a8dfcd22d1473fcbcfebe60e9
Merey <kemelm2004@gmail.com>
2024-03-28 12:12:39 +0600

    done

---
+++ Lab7/task1/3/list-1/1.py
@@ -0,0 +1,3 @@
+def first_last6(nums):
+     x=len(nums)-1
+     return nums[0]==6 or nums[x]==6
---
+++ Lab7/task1/3/list-1/2.py
@@ -0,0 +1,3 @@
+def same_first_last(nums):
+  x=len(nums)-1
+  return len(nums)>=1 and nums[0]==nums[x]
